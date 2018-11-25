496473b5c3e9a21ee305198c798ed0b83577b2e6
Sujoy Datta <sujoydatta26@gmail.com>
2018-11-25 15:48:38 +0530

    Added Yellow Ball and score - 25/11/2018

--- app/src/main/java/com/morningstar/flyingfishgame/FlyingFishView.java
+++ app/src/main/java/com/morningstar/flyingfishgame/FlyingFishView.java
@@ -15,37 +15,112 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.view.MotionEvent;
 import android.view.View;
 
 class FlyingFishView extends View {
-    private Bitmap fishBitmap;
+    private Bitmap fishBitmap[] = new Bitmap[2];
     private Bitmap backgroundImage;
-    private Paint score = new Paint();
+    private Paint scorePaint = new Paint();
     private Bitmap life[] = new Bitmap[2];
+
+    private int fishXAxis = 30;
+    private int fishYAxis;
+    private int fishSpeed;
+
+    private Paint yellowPaint;
+    private Paint redPaint;
+    private int yellowXAxis, yellowYAxis, yellowSpeed;
+    private int redXAxis, redYAxis, redSpeed;
+
+    private int canvasHeight;
+    private int canvasWidth;
+
+    private boolean hasTouched = false;
+    private int score;
 
     public FlyingFishView(Context context) {
         super(context);
-        fishBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.fish1);
+        fishBitmap[0] = BitmapFactory.decodeResource(getResources(), R.drawable.fish1);
+        fishBitmap[1] = BitmapFactory.decodeResource(getResources(), R.drawable.fish2);
+
         backgroundImage = BitmapFactory.decodeResource(getResources(), R.drawable.background);
-        score.setColor(Color.WHITE);
-        score.setTextSize(70f);
-        score.setTypeface(Typeface.DEFAULT_BOLD);
-        score.setAntiAlias(true);
+
+        scorePaint.setColor(Color.WHITE);
+        scorePaint.setTextSize(70f);
+        scorePaint.setTypeface(Typeface.DEFAULT_BOLD);
+        scorePaint.setAntiAlias(true);
 
         life[0] = BitmapFactory.decodeResource(getResources(), R.drawable.hearts);
         life[1] = BitmapFactory.decodeResource(getResources(), R.drawable.heart_grey);
+
+        fishYAxis = 550;
+
+        yellowPaint = new Paint();
+        yellowPaint.setColor(Color.YELLOW);
+        yellowPaint.setAntiAlias(false);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        //Get the height and width of the canvas
+        canvasHeight = getHeight();
+        canvasWidth = getWidth();
         canvas.drawBitmap(backgroundImage, 0, 0, null);
-        canvas.drawBitmap(fishBitmap, 0, 600, null);
-        canvas.drawText("Score: ", 30, 60, score);
 
-        canvas.drawBitmap(life[0], 580, 10, null);
-        canvas.drawBitmap(life[0], 680, 10, null);
-        canvas.drawBitmap(life[0], 780, 10, null);
+        int minFishY = fishBitmap[0].getHeight();
+        int maxFishY = canvasHeight - (minFishY * 3);
+        fishYAxis = fishYAxis + fishSpeed;
+        if (fishYAxis < minFishY) {
+            fishYAxis = minFishY;
+        }
+        if (fishYAxis > maxFishY) {
+            fishYAxis = maxFishY;
+        }
+
+        fishSpeed = fishSpeed + 2;
+
+        if (hasTouched) {
+            canvas.drawBitmap(fishBitmap[1], fishXAxis, fishYAxis, null);
+            hasTouched = false;
+        } else {
+            canvas.drawBitmap(fishBitmap[0], fishXAxis, fishYAxis, null);
+        }
+
+        yellowSpeed = 15;
+
+        if (yellowXAxis < 0) {
+            yellowXAxis = canvasWidth + 20;
+            yellowYAxis = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
+        }
+
+        yellowXAxis = yellowXAxis - yellowSpeed;
+
+        if (isBallHit(yellowXAxis, yellowYAxis)) {
+            score += 10;
+            yellowYAxis = -100;
+        }
+
+        canvas.drawCircle(yellowXAxis, yellowYAxis, 20, yellowPaint);
+
+        canvas.drawText("Score: " + score, 30, 60, scorePaint);
+        canvas.drawBitmap(life[0], canvasWidth - 300, 10, null);
+        canvas.drawBitmap(life[0], canvasWidth - 200, 10, null);
+        canvas.drawBitmap(life[0], canvasWidth - 100, 10, null);
+    }
+
+    private boolean isBallHit(int x, int y) {
+        return fishXAxis < x && x < (fishXAxis + fishBitmap[0].getWidth()) && fishYAxis < y && y < (fishYAxis + fishBitmap[0].getHeight());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            hasTouched = true;
+            fishSpeed = -22;
+        }
+        return hasTouched;
     }
 }
--- app/src/main/java/com/morningstar/flyingfishgame/MainActivity.java
+++ app/src/main/java/com/morningstar/flyingfishgame/MainActivity.java
@@ -1,15 +1,35 @@
 package com.morningstar.flyingfishgame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private FlyingFishView fishView;
+    private final int INTERVAL = 25;
+    private Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         fishView = new FlyingFishView(this);
         setContentView(fishView);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        fishView.invalidate();
+                    }
+                });
+            }
+        }, 0, INTERVAL);
     }
 }
