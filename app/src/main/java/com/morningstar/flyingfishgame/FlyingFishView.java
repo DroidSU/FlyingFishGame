ecc900a7aa241ce5a6c9035a9995304311eb7c41
Sujoy Datta <sujoydatta26@gmail.com>
2018-11-26 09:52:50 +0530

    Game Finished. Bugs to be fixed - 25/11/2018

--- app/src/main/java/com/morningstar/flyingfishgame/FlyingFishView.java
+++ app/src/main/java/com/morningstar/flyingfishgame/FlyingFishView.java
@@ -8,7 +8,9 @@
 
 package com.morningstar.flyingfishgame;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -30,14 +32,18 @@ class FlyingFishView extends View {
 
     private Paint yellowPaint;
     private Paint redPaint;
+    private Paint greenPaint;
+
     private int yellowXAxis, yellowYAxis, yellowSpeed;
     private int redXAxis, redYAxis, redSpeed;
+    private int greenXAxis, greenYAxis, greenSpeed;
 
     private int canvasHeight;
     private int canvasWidth;
 
     private boolean hasTouched = false;
     private int score;
+    private int life_count;
 
     public FlyingFishView(Context context) {
         super(context);
@@ -55,10 +61,19 @@ class FlyingFishView extends View {
         life[1] = BitmapFactory.decodeResource(getResources(), R.drawable.heart_grey);
 
         fishYAxis = 550;
+        life_count = 3;
 
         yellowPaint = new Paint();
         yellowPaint.setColor(Color.YELLOW);
         yellowPaint.setAntiAlias(false);
+
+        greenPaint = new Paint();
+        greenPaint.setColor(Color.GREEN);
+        greenPaint.setAntiAlias(false);
+
+        redPaint = new Paint();
+        redPaint.setColor(Color.RED);
+        redPaint.setAntiAlias(false);
     }
 
     @Override
@@ -80,7 +95,7 @@ class FlyingFishView extends View {
             fishYAxis = maxFishY;
         }
 
-        fishSpeed = fishSpeed + 2;
+        fishSpeed += 2;
 
         if (hasTouched) {
             canvas.drawBitmap(fishBitmap[1], fishXAxis, fishYAxis, null);
@@ -89,26 +104,71 @@ class FlyingFishView extends View {
             canvas.drawBitmap(fishBitmap[0], fishXAxis, fishYAxis, null);
         }
 
-        yellowSpeed = 15;
-
-        if (yellowXAxis < 0) {
-            yellowXAxis = canvasWidth + 20;
-            yellowYAxis = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
-        }
-
-        yellowXAxis = yellowXAxis - yellowSpeed;
-
-        if (isBallHit(yellowXAxis, yellowYAxis)) {
-            score += 10;
-            yellowYAxis = -100;
-        }
-
-        canvas.drawCircle(yellowXAxis, yellowYAxis, 20, yellowPaint);
+        setUpYellowBall(canvas, minFishY, maxFishY);
+        setUpGreenball(canvas, minFishY, maxFishY);
+        setUpRedBall(canvas, minFishY, maxFishY);
 
         canvas.drawText("Score: " + score, 30, 60, scorePaint);
-        canvas.drawBitmap(life[0], canvasWidth - 300, 10, null);
-        canvas.drawBitmap(life[0], canvasWidth - 200, 10, null);
-        canvas.drawBitmap(life[0], canvasWidth - 100, 10, null);
+
+        for (int i = 3; i > 0; i--) {
+            int x = canvasWidth - (i * 100);
+            int y = 10;
+            if (i > life_count) {
+                canvas.drawBitmap(life[1], x, y, null);
+            } else
+                canvas.drawBitmap(life[0], x, y, null);
+        }
+    }
+
+    private void setUpRedBall(Canvas canvas, int minFishY, int maxFishY) {
+        redSpeed = 30;
+        if (redXAxis < 0) {
+            redXAxis = canvasWidth + 5;
+            redYAxis = (int) Math.floor(Math.random() * (maxFishY - minFishY) + minFishY);
+        }
+
+        redXAxis -= redSpeed;
+        if (isBallHit(redXAxis, redYAxis)) {
+            redXAxis -= 100;
+//            score-=20;
+            life_count -= 1;
+
+            if (life_count == 0) {
+                @SuppressLint("DrawAllocation") Intent intent = new Intent(getContext(), GameOverActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getContext().startActivity(intent);
+            }
+        }
+        canvas.drawCircle(redXAxis, redYAxis, 20, redPaint);
+    }
+
+    private void setUpGreenball(Canvas canvas, int minFishY, int maxFishY) {
+        greenSpeed = 30;
+        if (greenXAxis < 0) {
+            greenXAxis = canvasWidth + 5;
+            greenYAxis = (int) Math.floor(Math.random() * (maxFishY - minFishY) + minFishY);
+        }
+
+        greenXAxis -= greenSpeed;
+        if (isBallHit(greenXAxis, greenYAxis)) {
+            greenXAxis -= 100;
+            score += 20;
+        }
+        canvas.drawCircle(greenXAxis, greenYAxis, 20, greenPaint);
+    }
+
+    private void setUpYellowBall(Canvas canvas, int minFishY, int maxFishY) {
+        yellowSpeed = 15;
+        if (yellowXAxis < 0) {
+            yellowXAxis = canvasWidth + 5;
+            yellowYAxis = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
+        }
+        yellowXAxis -= yellowSpeed;
+        if (isBallHit(yellowXAxis, yellowYAxis)) {
+            score += 10;
+            yellowXAxis = -100;
+        }
+        canvas.drawCircle(yellowXAxis, yellowYAxis, 20, yellowPaint);
     }
 
     private boolean isBallHit(int x, int y) {
---
+++ app/src/main/java/com/morningstar/flyingfishgame/GameOverActivity.java
@@ -0,0 +1,36 @@
+/*
+ * Created by Sujoy Datta. Copyright (c) 2018. All rights reserved.
+ *
+ * To the person who is reading this..
+ * When you finally understand how this works, please do explain it to me too at sujoydatta26@gmail.com
+ * P.S.: In case you are planning to use this without mentioning me, you will be met with mean judgemental looks and sarcastic comments.
+ */
+
+package com.morningstar.flyingfishgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    private Button startAgain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        startAgain = findViewById(R.id.startAgainButton);
+
+        startAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GameOverActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
